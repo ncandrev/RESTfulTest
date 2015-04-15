2abd331f1662992e1b5e057474aa9b69aa80267d
unknown <NCANDREV@DJ5X1Q02.Cablevision.com>
2015-04-15 16:33:12 +0100

    Added changes to controller

--- src/main/java/com/restful/controllers/CustomerController.java
+++ src/main/java/com/restful/controllers/CustomerController.java
@@ -1,14 +1,25 @@
 package com.restful.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.restful.json.CustomerJSON;
+
 @Controller
 public class CustomerController {
+	
+	private static ArrayList<CustomerJSON> customers = new ArrayList<CustomerJSON>();
+	
+	static {
+		customers.add(new CustomerJSON("Jon", "Snow"));
+		customers.add(new CustomerJSON("Tyrion", "Lanister"));
+	}
 
 	@RequestMapping(value = "/customers")
-	public @ResponseBody String getAllCustomers() {
-		return "ALL CUSTOMERS";
+	public @ResponseBody ArrayList<CustomerJSON> getAllCustomers() {
+		return customers;
 	}
 }
