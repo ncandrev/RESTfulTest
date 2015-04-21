98e496a966c198f3fae9863fa00f3c3720e4d26c
unknown <NCANDREV@DJ5X1Q02.Cablevision.com>
2015-04-21 14:24:47 +0100

    Added comments to the controller

--- src/main/java/com/restful/controllers/CustomerController.java
+++ src/main/java/com/restful/controllers/CustomerController.java
@@ -8,9 +8,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.restful.json.CustomerJSON;
 
+/**
+ * Customer Service.
+ * 
+ * @author NCANDREV
+ *
+ */
 @Controller
 public class CustomerController {
 	
+	//This will hold all of the customers.
 	private static ArrayList<CustomerJSON> customers = new ArrayList<CustomerJSON>();
 	
 	static {
